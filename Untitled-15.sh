721c770910dd188d7a54fc8234070b8e97c84196
AcefortuneRBH <95355852+AcefortuneRBH@users.noreply.github.com>
2025-04-12 17:50:51 -0400

    Update Untitled-15.sh

--- Untitled-15.sh
+++ Untitled-15.sh
@@ -1,2 +1,2 @@
 
-mv /Users/timxayamoungkhoun/MHDGLOBAL/layer2_rollup.cpp /Users/timxayamoungkhoun/MHDGLOBAL/layer2_rollup.rs
+mv /Users/timxayamoungkhoun/MHDGLOBAL/layer2_rollup.cpp /Users/timxayamoungkhoun/MHD/layer2_rollup.rs
